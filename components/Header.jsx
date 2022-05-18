8357c42bd35ffd0798a1dba5755a58500fd636ff
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-18 19:16:08 +0100

    new

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,14 +17,14 @@ import HeaderIcon from './HeaderIcon'
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
       <div className="flex items-center">
         <Image src="/assets/fb.png" width="40" height="40" layout="fixed" />
         <div className="flex ml-2 items-center rounded-full bg-gray-100">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="search..."
           />
@@ -43,6 +43,8 @@ function Header() {
       </div>
 
       {/* right */}
+      <div>{/* image */}</div>
+      <p className="whitespace-nowrap font-semibold pr-3">Ayoub Maimmadi</p>
     </div>
   )
 }
