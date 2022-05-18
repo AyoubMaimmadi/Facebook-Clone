d9408bfc039a81992124227518d7646a8a935ee2
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-18 19:00:58 +0100

    new

--- components/Header.jsx
+++ components/Header.jsx
@@ -33,8 +33,12 @@ function Header() {
 
       {/* center */}
       <div className="flex justify-center flex-grow">
-        <div>
+        <div className="flex space-x-6 md:space-x-2">
           <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
 
--- components/HeaderIcon.jsx
+++ components/HeaderIcon.jsx
@@ -1,7 +1,7 @@
 function HeaderIcon({ Icon }) {
   return (
-    <div>
-      <Icon className="" />
+    <div className="flex cursor-pointer items-center md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500">
+      <Icon className="h-5 " />
     </div>
   )
 }
