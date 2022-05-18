0dfc2746d51cc78401b4c721f6223d26790f8ca1
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 00:56:16 +0100

    new

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Stories from './Stories'
+import Inputbox from './Inputbox'
 
 const Feed = () => {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories />
-        {/* Inputbox */}
+        <Inputbox />
         {/* Posts */}
       </div>
     </div>
