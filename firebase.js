2b3ff04e35e9e466b3d58633838cba77e5287170
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 14:27:57 +0100

    new

--- firebase.js
+++ firebase.js
@@ -13,3 +13,8 @@ const firebaseConfig = {
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app()
+
+const db = app.firestore()
+const storage = firebase.storage()
+
+export { db, storage }
