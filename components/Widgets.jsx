f3e464ef5d67abed87ab09d5ca0cfadb38801e8d
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 15:49:41 +0100

    new

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,6 +1,6 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import { DotsHorizontalIcon, VideoCameraIcon } from '@heroicons/react/solid'
-import React from 'react'
+import Contact from './Contact'
 
 const Widgets = () => {
   const contacts = [
@@ -11,7 +11,22 @@ const Widgets = () => {
     { src: '/assets/ayoub.jpg', name: 'Mark Zuckerberg' },
   ]
 
-  return <div>ayoub</div>
+  return (
+    <div className="hidden lg:flex flex-col w-60 p-2 mt-5">
+      <div className="flex justify-between items-center text-gray-500 mb-5">
+        <h2 className="text-xl">Contacts</h2>
+        <div className="flex space-x-2">
+          <VideoCameraIcon className="h-6" />
+          <SearchIcon className="h-6" />
+          <DotsHorizontalIcon className="h-6" />
+        </div>
+      </div>
+
+      {contacts.map((contact) => (
+        <Contact key={contact.src} src={contact.src} name={contact.name} />
+      ))}
+    </div>
+  )
 }
 
 export default Widgets
