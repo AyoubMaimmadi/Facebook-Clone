f61dae7e294d15835d93128be0a99a46121035d0
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 18:26:49 +0100

    new

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,7 +21,13 @@ function Header() {
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       <div className="flex items-center">
-        <Image src="/assets/fb.png" width="40" height="40" layout="fixed" />
+        <Image
+          src="/assets/fb.png"
+          width="40"
+          height="40"
+          layout="fixed"
+          alt="image"
+        />
         <div className="hidden md:inline-flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
@@ -50,6 +56,7 @@ function Header() {
           width="40"
           height="40"
           layout="fixed"
+          alt="image"
         />
       </div>
       <p className="whitespace-nowrap font-semibold pr-3 ml-2">
--- components/StoryCard.jsx
+++ components/StoryCard.jsx
@@ -10,11 +10,13 @@ const StoryCard = ({ name, src, profile }) => {
         height={40}
         layout="fixed"
         objectFit="cover"
+        alt="image"
       />
       <Image
         className="object-cover filter brightness-75 rounded-full lg:rounded-3xl"
         src={src}
         layout="fill"
+        alt="image"
       />
       <p className="absolute opacity-0 lg:opacity-100 bottom-4 w-5/6 text-white text-sm font-bold truncate">
         {name}
