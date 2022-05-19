5a5cdebcbdcd7f05203833f4842b7728e14dfcdf
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 15:40:59 +0100

    new

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -3,6 +3,14 @@ import { DotsHorizontalIcon, VideoCameraIcon } from '@heroicons/react/solid'
 import React from 'react'
 
 const Widgets = () => {
+  const contacts = [
+    { src: '/assets/ayoub.jpg', name: 'Ayoub Maimmadi' },
+    { src: '/assets/ayoub.jpg', name: 'Jeff Bezoz' },
+    { src: '/assets/ayoub.jpg', name: 'Elon Musk' },
+    { src: '/assets/ayoub.jpg', name: 'Bill Gates' },
+    { src: '/assets/ayoub.jpg', name: 'Mark Zuckerberg' },
+  ]
+
   return <div>ayoub</div>
 }
 
