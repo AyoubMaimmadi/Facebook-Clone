24de098f5037fd8d0f020181483eadfefa9e4e35
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 15:05:12 +0100

    new

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
+import { ChatAltIcon, ShareIcon, ThumbUpIcon } from '@heroicons/react/outline'
 
-const Post = () => {
+const Post = ({ name, message, email, postImage, image, timestamp }) => {
   return <div>Post</div>
 }
 
