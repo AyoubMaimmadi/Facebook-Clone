6a7f3f65616af7b6234a502242fba86c85bec64c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 16:19:56 +0100

    new

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,10 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   providers: [
     Providers.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      // clientId: process.env.FACEBOOK_CLIENT_ID,
+      // clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientId: '329689172571660',
+      clientSecret: 'ddcf1e05e2c656c6f832884c9930a122',
     }),
   ],
 })
