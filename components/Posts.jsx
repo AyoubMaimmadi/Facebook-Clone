b99cd5c8227b768dde8dfe338811f7dbea242f35
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 14:59:02 +0100

    new

---
+++ components/Posts.jsx
@@ -0,0 +1,12 @@
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '../firebase'
+
+function Posts() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Posts
