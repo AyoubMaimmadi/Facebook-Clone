c784cdeb4d4db2790d817fd6ca66026cfed76642
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 15:04:41 +0100

    new

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,7 @@
-import Image from next/image
+import Image from 'next/image'
 
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
+  return <div>Post</div>
 }
 
 export default Post
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,5 +1,6 @@
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { db } from '../firebase'
+import Post from './Post'
 
 function Posts() {
   const [realtimePosts, loading, error] = useCollection(
@@ -8,7 +9,29 @@ function Posts() {
 
   return (
     <div>
-      <div>ayoub</div>
+      {realtimePosts
+        ? realtimePosts?.docs.map((post) => (
+            <Post
+              key={post.id}
+              name={post.data().name}
+              message={post.data().message}
+              email={post.data().email}
+              timestamp={post.data().timestamp}
+              image={post.data().image}
+              postImage={post.data().postImage}
+            />
+          ))
+        : posts.map((post) => (
+            <Post
+              key={post.id}
+              name={post.name}
+              message={post.message}
+              email={post.email}
+              timestamp={post.timestamp}
+              image={post.image}
+              postImage={post.postImage}
+            />
+          ))}
     </div>
   )
 }
