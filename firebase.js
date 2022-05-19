0e07f74a177b734c6fdeecb71ef55b443b27af47
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-19 14:26:17 +0100

    new

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDKBILLptsAHmxWSnL9TOZuNl0go6mhiM0',
+  authDomain: 'ayoub-fb-clone.firebaseapp.com',
+  projectId: 'ayoub-fb-clone',
+  storageBucket: 'ayoub-fb-clone.appspot.com',
+  messagingSenderId: '244172357601',
+  appId: '1:244172357601:web:b82e648582c89389329ca0',
+}
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app()
